be31b3eb9dae416d9060faacfe40c1ae57f6ec2b
GuruTavare <gurutavare192@gmail.com>
2023-10-03 15:16:58 +0530

    new commit

--- src/com/prowings/loops/ForLoopDemo.java
+++ src/com/prowings/loops/ForLoopDemo.java
@@ -3,10 +3,19 @@ package com.prowings.loops;
 public class ForLoopDemo {
 
 	public static void main(String[] args) {
-		
-		for(int i=0; i<=5 ;i++) {
-			
-			System.out.println(i);
+
+		for (int i = 0; i <= 2; i++) {
+			System.out.println("    hey   ");
+
+			for (int j = 0; j <= 2; j++) {
+				System.out.println("j ");
+
+				for (int k = 0; k <= 2; k++) {
+					System.out.println("    k ");
+				}
+
+			}
 		}
+
 	}
 }
