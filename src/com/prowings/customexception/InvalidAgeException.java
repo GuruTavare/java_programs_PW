489cb01e6e860ea993e9f9eb9be8b1edf5456b7c
GuruTavare <gurutavare192@gmail.com>
2023-12-11 15:29:18 +0530

    new commit 11/12/23

---
+++ src/com/prowings/customexception/InvalidAgeException.java
@@ -0,0 +1,21 @@
+package com.prowings.customexception;
+
+public class InvalidAgeException extends RuntimeException {
+
+	public InvalidAgeException() {
+		super();
+	}
+
+	public InvalidAgeException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public InvalidAgeException(String message) {
+		super(message);
+	}
+
+	public InvalidAgeException(Throwable cause) {
+		super(cause);
+	}
+
+}
