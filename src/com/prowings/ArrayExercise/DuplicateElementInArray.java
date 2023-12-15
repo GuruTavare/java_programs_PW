aedeb2571225554769881f2638741bf82a287a42
GuruTavare <gurutavare192@gmail.com>
2023-12-15 22:33:06 +0530

    new commit

--- src/com/prowings/ArrayExercise/DuplicateElementInArray.java
+++ src/com/prowings/ArrayExercise/DuplicateElementInArray.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class DuplicateElementInArray {
 	public static void main(String[] args) {
-		int[] numbers = { 1, 2, 3, 4, 2, 7, 8, 3, 1, 9 };
+		int[] numbers = { 1, 2, 3, 4, 2, 2, 2, 2, 2, 2, 7, 8, 3, 1, 9 };
 		int[] duplicates = findDuplicates(numbers);
 
 		System.out.println("Duplicate elements: " + Arrays.toString(duplicates));
@@ -12,7 +12,7 @@ public class DuplicateElementInArray {
 
 	private static int[] findDuplicates(int[] array) {
 		Arrays.sort(array);
-	//	System.out.println(Arrays.toString(array));
+		// System.out.println(Arrays.toString(array));
 		int[] duplicates = new int[array.length];
 
 		int count = 0;
@@ -21,9 +21,12 @@ public class DuplicateElementInArray {
 			if (array[i] == array[i - 1]) {
 				duplicates[count] = array[i];
 				count++;
+				while (i < array.length - 1 && array[i] == array[i + 1]) {
+					i++;
+				}
 			}
 		}
-	//	System.out.println(Arrays.toString(duplicates));
+		// System.out.println(Arrays.toString(duplicates));
 		return Arrays.copyOf(duplicates, count);
 	}
 }
